8ecae4a2eab1bf4efb17b24a6cfac2ee5c418e71
Tariq Hook <thook@Tariqs-MacBook-Pro.local>
2020-11-21 10:47:57 -0500

    First Checkin

---
+++ src/main/java/com/codedifferently/walkthrough/vendingmachine/VendingMachine.java
@@ -0,0 +1,4 @@
+package com.codedifferently.walkthrough.vendingmachine;
+
+public class VendingMachine {
+}
---
+++ src/test/java/com/codedifferently/walkthrough/vendingmachine/VendingMachineTest.java
@@ -0,0 +1,11 @@
+package com.codedifferently.walkthrough.vendingmachine;
+
+import org.apache.log4j.Logger;
+
+public class VendingMachineTest {
+    private final static Logger logger = Logger.getLogger(VendingMachineTest.class);
+
+    public static void main(String[] args) {
+        logger.info("Start");
+    }
+}
